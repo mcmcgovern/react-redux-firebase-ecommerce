675ed24e4420077f546322162f3298895ed3b90b
mcmcgovern <michaelmcgovern120@gmail.com>
2023-01-22 15:36:41 -0500

    basic header and logo setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
+import "./default.scss";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <h2>Hello world!</h2>
     </div>
   );
