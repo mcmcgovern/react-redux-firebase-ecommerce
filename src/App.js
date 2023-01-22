b1e81e5c8b4dbfec64730ed4342bd5cede3b4bee
mcmcgovern <michaelmcgovern120@gmail.com>
2023-01-22 16:14:25 -0500

    set up directory with hero images

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./default.scss";
 import Header from "./components/Header";
+import Homepage from "./pages/Homepage";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <h2>Hello world!</h2>
+      <div className="main">
+        <Homepage />
+      </div>
     </div>
   );
 }
